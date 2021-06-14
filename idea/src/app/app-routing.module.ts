1b8b152473e42b4055f0a1f6e73732a21cb5a6b1
Gaurav Pratap Singh <gpdotsingh@gmail.com>
2021-06-15 00:43:47 +0200

    Try to add oauth2 configuration

--- idea/src/app/app-routing.module.ts
+++ idea/src/app/app-routing.module.ts
@@ -1,15 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { GetHealthComponent } from './components/get-health/get-health.component';
+import { HomeComponent } from './home/home/home.component';
+import { CallbackComponent } from './login/callback/callback.component';
+import { LoginComponent } from './login/login/login.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'',pathMatch:'full'},
   
   {
       path:"health",
-      component:GetHealthComponent
+      component:GetHealthComponent,
+      canActivate:[AuthGuard]
+  },
+  {
+      path:"login",
+      component:LoginComponent
+      
+  },
+  {
+    path:"callback",
+    component:CallbackComponent
+    
   }
-  
+  ,
+  {
+    path:"home",
+    component:HomeComponent
+    
+  }
 ];
 
 @NgModule({
--- idea/src/app/app.module.ts
+++ idea/src/app/app.module.ts
@@ -4,19 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GetHealthComponent } from './components/get-health/get-health.component';
-import {HttpClientModule } from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoginComponent } from './login/login/login.component';
+import { CallbackComponent } from './login/callback/callback.component';
+import { AuthHeaderInterceptor } from 'src/AuthHeaderInterceptor';
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GetHealthComponent
+    GetHealthComponent,
+    LoginComponent,
+    CallbackComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:AuthHeaderInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
